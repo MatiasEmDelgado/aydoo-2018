8521fe78698094d41941a7d651be1df8d7823ef1
Matuchinn <matias.untref@gmail.com>
2018-05-05 04:00:49 -0300

    Agregado test y logica para permitir al cliente obtener una suscripcion

--- libreria/src/test/java/ar/edu/untref/aydoo/LibreriaTest.java
+++ libreria/src/test/java/ar/edu/untref/aydoo/LibreriaTest.java
@@ -40,17 +40,18 @@ public class LibreriaTest {
         Assert.assertEquals(24.2, montoDeCobroEnEneroParaPepe, 1);
     }
 
-    @Ignore
+    @Test
     public void elClienteAdquiereUnaSuscripcionEnEneroParaAbril2018() {
         Cliente pepe = new Cliente("Pepe", "Grillo", "Lavalle 430");
         Mes abril2018 = new Mes(4, 2018);
         List<Mes> mesesDeLaSuscripcion = new ArrayList<Mes>();
         mesesDeLaSuscripcion.add(abril2018);
-        Producto elGrafico = new Suscripcion(20.0, 2, mesesDeLaSuscripcion);
+        Producto elGrafico = new Suscripcion(100.0, 2, mesesDeLaSuscripcion);
         Libreria libreria = new Libreria ();
         Mes enero2018 = new Mes(1, 2018);
         libreria.registrarCompra(elGrafico, pepe, enero2018);
         Double montoDeCobroEnEneroParaPepe = libreria.obtenerMontoDeCobro(enero2018, pepe);
-        Assert.assertEquals(24.2, montoDeCobroEnEneroParaPepe, 1);
+        Assert.assertEquals(100.0, montoDeCobroEnEneroParaPepe, 1);
     }
+
 }
